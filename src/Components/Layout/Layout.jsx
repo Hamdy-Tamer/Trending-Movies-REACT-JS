fc72ce2508d386dc6b8eebb90c51a70561a8ce68
Hamdy-Tamer <hamdytamer253@gmail.com>
2025-10-17 14:53:53 +0300

    Update Layout component to conditionally render Footer
    Added logic to conditionally hide the footer based on the current route and login status.

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,21 @@
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function Layout({logindata, setLoginData}) {
-  return <>
-  <Navbar logindata = {logindata} setLoginData = {setLoginData}/>
-  <Outlet></Outlet>
-  <Footer/>
+  const location = useLocation();
   
-  </>
-}    
+  // Hide footer on login and register pages
+  const hideFooter = location.pathname === '/login' || 
+                    location.pathname === '/register' || 
+                    location.pathname === '/';
+
+  return (
+    <>
+      <Navbar logindata={logindata} setLoginData={setLoginData}/>
+      <Outlet/>
+      {/* Show footer only if not on login/register pages AND user is logged in */}
+      {!hideFooter && logindata && <Footer/>}
+    </>
+  );
+}
